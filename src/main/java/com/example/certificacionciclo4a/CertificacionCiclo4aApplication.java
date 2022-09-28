dbd7441344052839a80343703d72dd1c4c46d873
Santiago Nohrá Nieto <89982255+santiagonohra@users.noreply.github.com>
2022-09-28 10:08:15 -0500

    Update CertificacionCiclo4aApplication.java

--- src/main/java/com/example/certificacionciclo4a/CertificacionCiclo4aApplication.java
+++ src/main/java/com/example/certificacionciclo4a/CertificacionCiclo4aApplication.java
@@ -24,9 +24,9 @@ public class CertificacionCiclo4aApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         productoRepository.deleteAll();
         productoRepository.saveAll(List.of(
-                new Producto(null, "Wilson Master 332", "Raqueta", "Raqueta de tenis marca Wilson", 250000, 10, true),
-                new Producto(null, "Nike FoamRunners", "Tenis", "Sneakers de Nike", 340000, 16, true),
-                new Producto(null, "Head Headband", "Misc", "Diadema absorbedora marca Head", 17000, 100, true)
+                new Producto(null, "Wilson Master 332", "Raqueta", "Raqueta de tenis marca Wilson","https://cdnmedia.racquets4u.com/media/catalog/product/cache/6e8e24cc73e2eff11526e72b5f24c033/w/i/wilson-ultra-100-ul-v3-tennis-racquet-260-g-unstrung_1.jpg", 250000, 10, true),
+                new Producto(null, "Nike FoamRunners", "Tenis", "Sneakers de Nike","https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d77c8f9a-c6e8-40a5-8332-910312bc86a5/blazer-mid-77-big-kids-shoes-4VfSTd.png", 340000, 16, true),
+                new Producto(null, "Head Headband", "Misc", "Diadema absorbedora marca Head","http://www.mauricerobinsonsports.com/images/pics/head_headband.jpg", 17000, 100, true)
         ));
 
     }
